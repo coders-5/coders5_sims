6002ceb253e95b85619934d12c4f48848c868067
dell <dell@pc.mshome.net>
2025-04-04 14:22:23 +0300

    initial set

---
+++ jsimsCore/src/domain/College.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author dell
+ */
+public class College {
+    int id;
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
